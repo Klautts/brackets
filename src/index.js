ff8ce1ad2e87b0375c0e16c55b64d8b510da17b4
Vlad Razin <47825278+Klautts@users.noreply.github.com>
2019-03-21 17:44:12 +0300

    add brackets task (3 taste filed)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var s = str.split(''),
+        stack = [],
+        open = ['{', '(', '['],
+        close = ['}', ')', ']'],
+        ci,
+        oi;
+    for (var i = 0; i < s.length; i++) {
+       oi = open.indexOf(s[i]);
+       if (oi !== -1) {
+           stack.push(oi);
+           continue;
+       }
+       ci = close.indexOf(s[i]);
+       if (ci !== -1) {
+           oi= stack.pop();
+           if (ci !== oi) {
+               return false;
+           }
+       }
+    }
+    if (stack.length !== 0) {
+        return false;
+    }
+    return true;
 }
