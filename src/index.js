13ce218bd0a5c47ddc38aa26b5a19e16ef92c7ee
Klautts <47825278+Klautts@users.noreply.github.com>
2019-03-24 22:10:08 +0300

    add my task

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
     var s = str.split(''),
         stack = [],
-        open = ['{', '(', '['],
-        close = ['}', ')', ']'],
+        open = ['{', '(', '[','1','3','5'],
+        close = ['}', ')', ']','2','4','6'],
+		netral = ['|','7','8'],
         ci,
-        oi;
+        oi,
+		kol=0,
+		net,
+		f=0;
     for (var i = 0; i < s.length; i++) {
+		net = netral.indexOf(s[i]);
+		if (net !== -1) {
+		   if(f===0)
+		   {
+			   stack.push(net);
+			   f=1;
+		   }
+		   else
+		   {
+				f=0;
+				stack.pop(net);
+		   }
+        continue;
+       }
        oi = open.indexOf(s[i]);
        if (oi !== -1) {
            stack.push(oi);
@@ -19,7 +37,7 @@ module.exports = function check(str, bracketsConfig) {
            }
        }
     }
-    if (stack.length !== 0) {
+    if (stack.length !== 0 || f===1) {
         return false;
     }
     return true;
